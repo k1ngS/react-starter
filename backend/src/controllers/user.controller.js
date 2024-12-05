6945018a8e69b9a5c4c916004244baa9633ae0e0
k1ngS <marcos.beltrao@outlook.pt>
2024-12-05 01:31:21 -0300

    :sparkles: feat: refactor user management by creating new user model and controller, removing old implementations

---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,7 @@
+// import { getAllUsers } from "../models/users.model.js";
+
+// export async function listUsers(req, res) {
+//   const users = await getAllUsers();
+
+//   res.status(200).json(users);
+// }
---
+++ backend/src/routes/user.route.js
@@ -0,0 +1,8 @@
+import express from "express";
+// import { listUsers } from "../controllers/users.controller.js";
+
+const router = express.Router();
+
+//router.get("/", listUsers)
+
+export default router;
