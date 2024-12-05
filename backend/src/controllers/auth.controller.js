705fd153b5d32fc4cded784e2c231d76e9d10f27
k1ngS <marcos.beltrao@outlook.pt>
2024-12-05 01:31:12 -0300

    :sparkles: feat: add authentication routes and controller for signup, login, and logout

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,53 @@
+import { User } from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import { generateVerificationToken } from "../utils/generateVerificationToken.js";
+import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
+
+export const signup = async (req,res) => {
+  const { email, password, name } = req.body;
+  
+  try {
+    if(!email || !password || !name) {
+      return res.status(403).json({ success: false, message: "All fields are required" });
+    }
+
+    const userAlreadyExists = await User.findOne({ email });
+    if(userAlreadyExists) {
+      return res.status(400).json({ success: true, message: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const verificationToken = generateVerificationToken();
+    
+    const user = new User({
+      email,
+      password: hashedPassword,
+      name,
+      verificationToken,
+      verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000 // 24 hours
+    })
+
+    await user.save();
+
+    generateTokenAndSetCookie(res, user._id);
+
+    res.status(201).json({ 
+      success: true, 
+      message: "User created successfully",
+      user: {
+        ...user._doc,
+        password: undefined
+      }
+    });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+}
+
+export const login = async (req,res) => {
+  res.send("login route");
+}
+
+export const logout = async (req,res) => {
+  res.send("logout route");
+}
