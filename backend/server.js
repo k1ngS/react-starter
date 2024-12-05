bf995a47ba0b28e7cd52d8227bd584a3cd86b3b2
k1ngS <marcos.beltrao@outlook.pt>
2024-12-05 01:30:48 -0300

    :sparkles: feat: update server configuration to include database connection and change default port to 5000

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
 import express from "express";
-import routes from "./src/routes/usersRoutes.js";
+import routes from "./src/routes/index.js";
+import { connectDB } from "./src/config/connectDB.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 routes(app);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server is running on port ${PORT}`);
 });
 
