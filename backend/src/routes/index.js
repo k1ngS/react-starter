094c496e77053e4c606593122a88fb6062b9593e
k1ngS <marcos.beltrao@outlook.pt>
2024-12-06 17:39:01 -0300

    :sparkles: feat: add cookie parser middleware for handling cookies

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+
 import userRoutes from "./user.route.js";
 import authRoutes from "./auth.route.js";
 
@@ -10,6 +12,7 @@ const corsOptions = {
 
 const routes = (app) => {
   app.use(express.json());
+  app.use(cookieParser());
   app.use(cors(corsOptions));
 
   app.use('/users', userRoutes);
