d267e1878bce34aedfaeb4768240961ceca0a955
k1ngS <marcos.beltrao@outlook.pt>
2024-12-06 17:38:29 -0300

    :sparkles: feat: add dotenv configuration for environment variables

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import routes from "./src/routes/index.js";
+
 import { connectDB } from "./src/config/connectDB.js";
+
 import dotenv from "dotenv";
 
 dotenv.config();
