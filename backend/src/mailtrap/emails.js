44247f145cb8567958027fbd314cbda7169b8f0c
k1ngS <marcos.beltrao@outlook.pt>
2024-12-06 17:38:43 -0300

    :sparkles: feat: add password reset email functionality with success notification

--- backend/src/mailtrap/emails.js
+++ backend/src/mailtrap/emails.js
@@ -1,4 +1,4 @@
-import { VERIFICATION_EMAIL_TEMPLATE } from "./emailTemplates.js";
+import { PASSWORD_RESET_REQUEST_TEMPLATE, PASSWORD_RESET_SUCCESS_TEMPLATE, VERIFICATION_EMAIL_TEMPLATE } from "./emailTemplates.js";
 import { mailtrapClient, sender } from "./mailtrap.config.js";
 
 export const sendVerificationEmail = async (email, verificationToken) => {
@@ -38,5 +38,40 @@ export const sendWelcomeEmail = async (email, name) => {
   } catch (error) {
     console.error(`Error sending welcome email: ${error.message}`);
     throw new Error(`Error sending welcome email: ${error.message}`);
+  }
+}
+
+export const sendResetPasswordEmail = async (email, resetURL) => {
+  const recipient = [{ email }];
+
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: recipient,
+      subject: "Reset your password",
+      html: PASSWORD_RESET_REQUEST_TEMPLATE.replace("{resetURL}", resetURL),
+      category: "Password Reset"
+    })
+  } catch (error) {
+    console.error(`Error sending reset password email: ${error.message}`);
+    throw new Error(`Error sending reset password email: ${error.message}`);
+  }
+}
+
+export const sendResetSuccessEmail = async (email) => {
+  const recipient = [{ email }];
+
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: recipient,
+      subject: "Password reset successful",
+      html: PASSWORD_RESET_SUCCESS_TEMPLATE,
+      category: "Password Reset"
+    })
+    console.log(`Reset success email sent successfully: ${response}`);
+  } catch (error) {
+    console.error(`Error sending reset success email: ${error.message}`);
+    throw new Error(`Error sending reset success email: ${error.message}`);
   }
 }
