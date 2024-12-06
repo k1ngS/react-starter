e6d9fa2272e515a2c90d1950e2694e838e946bd8
k1ngS <marcos.beltrao@outlook.pt>
2024-12-06 17:46:40 -0300

    :bug: fix: update login response status code for invalid credentials

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -100,7 +100,7 @@ export const login = async (req, res) => {
   try {
     const user = await User.findOne({ email });
     if(!user) {
-      return res.status(400).json({ success: false, message: "Invalid credentials" });
+      return res.status(401).json({ success: false, message: "Invalid credentials" });
     }
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if(!isPasswordValid) {
