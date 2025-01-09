52a3d9b89ad1f73c477212d10d598f30d82485ec
k1ngS <marcos.beltrao@outlook.pt>
2025-01-09 01:44:02 -0300

    :art: feat: implement protected routes and redirect for authenticated users; add EmailVerificationPage and Toaster component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,85 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import FloatingShape from "./components/FloatingShape";
 
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
+import EmailVerificationPage from "./pages/EmailVerificationPage";
+import { Toaster } from "react-hot-toast";
+import { useAuthStore } from "./store/authStore";
+import { useEffect } from "react";
+import HomePage from "./pages/HomePage";
+
+// protect routes that require authentication
+const ProtectedRoute = ({children}) => {
+  const { isAuthenticated, user } = useAuthStore();
+
+  if(!isAuthenticated){
+    return <Navigate to="/login" replace />
+  }
+
+  if(!user.isVerified) {
+    return <Navigate to="/verify-email" replace />
+  }
+
+  return children;
+}
+
+// redirect authenticated users to the home page
+const RedirectAuthenticatedUser = ({children}) => {
+  const {isAuthenticated, user} = useAuthStore();
+
+  if(isAuthenticated && user.isVerified) {
+    return <Navigate to="/" replace />
+  }
+
+  return children;
+}
 
 function App() {
+  const { checkAuth , isAuthenticated, user } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth()
+  }, [checkAuth])
+
+  console.log("isAuthenticaded", isAuthenticated)
+  console.log("user", user)
 
   return (
-    <div className="
-        min-h-screen 
-        bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900 
-        flex items-center justify-center relative overflow-hidden"
+    <div className="relative flex items-center justify-center min-h-screen overflow-hidden bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900"
     >
       <FloatingShape color="bg-green-500" size="w-64 h-64" top="-5%" left="10%" delay={0} />
       <FloatingShape color="bg-emerald-500" size="w-48 h-48" top="70%" left="80%" delay={5} />
       <FloatingShape color="bg-lime-500" size="w-32 h-32" top="40%" left="-10%" delay={2} />
 
       <Routes>
-        <Route path="/" element={"Home"} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route 
+          path="/"
+          element={
+            <ProtectedRoute>
+              <HomePage />
+            </ProtectedRoute>
+          } 
+        />
+        <Route 
+          path="/signup" 
+          element={
+            <RedirectAuthenticatedUser>
+              <SignUpPage />
+            </RedirectAuthenticatedUser>
+          } 
+        />
+        <Route 
+          path="/login"
+          element={
+            <RedirectAuthenticatedUser>
+              <LoginPage />
+            </RedirectAuthenticatedUser>
+          }  
+        />
+        <Route path="/verify-email" element={<EmailVerificationPage />} />
       </Routes>
+      <Toaster />
     </div>
   );
 }
