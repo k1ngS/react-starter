0f5021c2983b0d39389883d49ffa211dc61d8f04
k1ngS <marcos.beltrao@outlook.pt>
2025-01-10 15:52:59 -0300

    :art: feat: add logout functionality to authStore for user session management

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -26,6 +26,16 @@ export const useAuthStore = create((set) => ({
       throw error;
     }
   },
+  logout: async() => {
+    set({ isLoading: true, error: null });
+    try {
+      await axios.post(`${API_URL}/logout`);
+      set({ user: null, isAuthenticated: false, error: null, isLoading: false });
+    } catch (error) {
+      set({ error: "Error logging out", isLoading: false });
+      throw error;
+    }
+  },
   signup: async(email, password, name) => {
     set({isLoading: true, error: null});
     try {
