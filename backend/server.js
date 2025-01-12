0c35a4bf25f3deef94845a974295df4da7720978
k1ngS <marcos.beltrao@outlook.pt>
2025-01-12 13:12:35 -0300

    :art: feat: serve frontend files in production and improve static file handling in server.js

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,24 @@ import routes from "./src/routes/index.js";
 import { connectDB } from "./src/config/connectDB.js";
 
 import dotenv from "dotenv";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve()
+
 routes(app);
 
-const PORT = process.env.PORT || 5000;
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  })
+}
+
 app.listen(PORT, () => {
   connectDB();
   console.log(`Server is running on port ${PORT}`);
