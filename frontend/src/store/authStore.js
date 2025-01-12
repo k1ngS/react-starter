3147b74700aa40f27277d775a98ce5955783c448
k1ngS <marcos.beltrao@outlook.pt>
2025-01-12 13:13:05 -0300

    :art: feat: add forgotPassword and resetPassword methods to auth store with error handling

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -11,6 +11,7 @@ export const useAuthStore = create((set) => ({
   error: null,
   isLoading: false,
   isCheckingAuth: true,
+  message: null,
   login: async(email, password) => {
     set({ isLoading: true, error: null });
     try {
@@ -65,5 +66,25 @@ export const useAuthStore = create((set) => ({
     } catch (error) {
       set({ error: null, isCheckingAuth: false, isAuthenticated: false });
     }
-  }
+  },
+  forgotPassword: async(email) => {
+    set({ isLoading: true, error: null });
+    try {
+      const response = await axios.post(`${API_URL}/forgot-password`, { email });
+      set({ message: response.data.message, isLoading: false });
+    } catch (error) {
+      set({ error: error.response.data.message || "Error sending reset password email", isLoading: false });
+      throw error;
+    }
+  },
+  resetPassword: async(token, password) => {
+    set({ isLoading: true, error: null });
+    try {
+      const response = await axios.post(`${API_URL}/reset-password/${token}`, { password });
+      set({ message: response.data.message, isLoading: false });
+    } catch (error) {
+      set({ error: error.response.data.message || "Error resetting password", isLoading: false });
+      throw error;
+    }
+  },
 }))
