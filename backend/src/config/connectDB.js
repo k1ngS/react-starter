298898190cb901d2b795ed9ab2c2311d2dc7d57d
k1ngS <marcos.beltrao@outlook.pt>
2025-01-13 19:43:06 -0300

    :bug: fix: increase maximum connection pool size for MongoDB

--- backend/src/config/connectDB.js
+++ backend/src/config/connectDB.js
@@ -5,6 +5,7 @@ export const connectDB = async () => {
     console.log("Connecting to MongoDB...");
     const connect = await mongoose.connect(process.env.MONGODB_URI, {
       connectTimeoutMS: 5000, // Tempo limite de 5 segundos
+      maxPoolSize: 5000, // Número máximo de conexões
     });
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (err) {
